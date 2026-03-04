497630f940d194a070143314318db92647e8d9ae
joseauggustto <jose.morais22aluno@gmail.com>
2026-03-04 10:23:10 -0300

    Quarta chuvosa

---
+++ Booleanos e condicionais.py
@@ -0,0 +1,35 @@
+# Hello. Hoje estudaremos valores booleanos(True ou False) e condicionais. 
+#Esse valroes servem para administrar o programa de acordo com certo valores condicionais.
+
+# Operadores de comparação: 
+# == se isso é igual aquilo 
+# !# se dois valores não são iguais
+# > se esse valor é maior que esse
+# < se esse valor é menor que esse
+# >= se esse valor é maior que esse ou igual
+# <= se esse valor é menor que esse ou igual
+
+print(3 > 5)
+print(5 < 9)
+print(5 == 5)
+print(5 != 5)
+
+# Em py a codicional mais básica é o if. Funciona assim:
+
+age = 19
+if age > 18:
+    print('Você tá fudido')
+
+altura = 1.80
+if altura >= 1.75:
+    print('Alto')
+
+#Quando a função if gera um 'false', aí então entra o comando else:
+
+peso = 74
+if peso >= 75:
+    print('Certo')
+
+
+
+
