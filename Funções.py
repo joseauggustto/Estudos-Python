ec20f3f7fe62a1e83f064874149671c7daeed49e
joseauggustto <jose.morais22aluno@gmail.com>
2026-03-07 15:09:52 -0300

    Tá dando certo aos poucos

--- Funções.py
+++ Funções.py
@@ -83,3 +83,84 @@ print(teste3)
 
 teste4 = analisar_vendas('Maria', 300)
 print(teste4)
+
+print('-' * 30)
+
+# Teste de velocidade
+
+def velocidade(kmh):
+    if kmh < 40:
+        return 'Muito lento, acelera!'
+    elif kmh <= 80:
+        return 'Velocidade ideal.'
+    else:
+        return 'Multado! Você está muito rápido.'
+        
+teste = velocidade(60)
+print(teste)
+
+print('-' * 30)
+
+# Sistema RPG
+
+def classificar_heroi_poder(poder):
+    if poder < 100:
+        return 'Recruta'
+    elif poder < 500:
+        return 'Guerreiro'
+    elif poder < 1000:
+        return 'Mestre'
+    else:
+        return 'Lenda'
+    
+testerpg = classificar_heroi_poder(99)
+print(testerpg)
+
+testerpg1 = classificar_heroi_poder(499)
+print(testerpg1)
+
+testerpg2 = classificar_heroi_poder(999)
+print(testerpg2)
+
+testerpg3 = classificar_heroi_poder(1001)
+print(testerpg3)
+
+print('-' * 30)
+
+# Desafio do Clima
+
+def temp_clima(temp):
+    if temp < 15:
+        return 'Está frio'
+    elif temp == 25:
+        return 'Temperatura Perfeita'
+    elif temp > 30:
+        return 'Está Quente'
+    else:
+        return 'Clima agradável'
+
+
+print('-' * 30)
+
+# Calculadora de Bônus
+
+def bonus_calculator(salario, tempo_casa):
+
+    if not isinstance(salario, (int, float)) or not isinstance(tempo_casa, (int, float)):
+        return 'Só pode números!'
+    
+    elif tempo_casa < 1:
+        return salario * 0
+    elif tempo_casa < 3:
+        return salario * 0.10
+    else:
+        return salario * 0.2
+    
+func1 = bonus_calculator('a', 0.5)
+print(func1)
+
+func2 = bonus_calculator(2000, 2)
+print(func2)
+
+func3 = bonus_calculator(2000, 5)
+print(func3)
