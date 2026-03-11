ed5b04ca6ab3726487975dbfe139f0e1b8723e91
joseauggustto <jose.morais22aluno@gmail.com>
2026-03-10 21:15:26 -0300

    Tá fluindo

---
+++ Exercícios Curso em Vídeo.py
@@ -0,0 +1,54 @@
+#print("Olá, Mundo!")   (Desafio 1 ok)
+
+print("-" * 30)
+
+#nome = input("Digite seu nome: ")
+#print("Olá", nome,", é um prazer em te conhecer!")   (Desafio 2 ok)
+
+print("-" * 30)
+
+#nome = input("Nome do funcionário: ")
+#salário = input("Salário do funcionário: ")  
+
+#print("O salário de:", nome, "é: R$", salário, ".")  (Desafio 3 ok, falta apenas colocar a forma do valor e ajeitar os espaços.)
+
+print("-" * 30)
+
+# n1 = int(input("Digite o primeiro número: "))
+# n2 = int(input("Digite o segundo número: "))
+# sum = n1 + n2
+# # print("A soma de", n1, "e", n2,"é:", sum)   
+
+#(Desafio 4 ok)
+
+print("-" * 30)
+
+# nota1 = float(input("Digite a primeira nota: "))
+# nota2 = float(input("Digite a segunda nota: "))
+
+# média = (nota1 + nota2) / 2
+# print("A sua média é:", média)   (Desafio 5 ok)
+
+print("-" * 30)
+
+# número = int(input("Digite qualquer número: "))
+# antecessor = número - 1 
+# sucessor = número + 1 
+# print("O antecessor de ", número, "é:", antecessor)
+# print("O sucessor de ", número, "é:", sucessor)          (Desafio 6 ok) 
+
+print("-" * 30)
+
+# number = float(input("Digite qualquer número: "))
+# dobro = number * 2
+# terca_parte = number / 3 
+# print("O dobro de", number, "é",dobro)
+# print("A terça parte de", number, "é", terca_parte)     (Desafio 7 ok)
+
+print("-" * 30)
+
+# distancia = float(input("Digite a distância em metros: "))
+# km = distancia / 1000
+# cm = distancia * 100
+# print("A distância em km de", distancia, "é:", km)
+# print("A distância em cm de", distancia, "é:", cm)   (Desafio 8 ok)
