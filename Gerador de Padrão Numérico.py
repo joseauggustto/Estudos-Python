e1511cd7e328d4f4901ee93836ac21ca3276e769
joseauggustto <jose.morais22aluno@gmail.com>
2026-04-05 15:02:20 -0300

    Tarde de domingo...

--- Gerador de Padrão Numérico.py
+++ Gerador de Padrão Numérico.py
@@ -1,1 +1,23 @@
 #Desafio freeCodeCamp - Conteúdo de Loops e Sequências.
+
+"""
+Você deve definir uma função chamada number_pattern que recebe um único parâmetro n (representando um número inteiro positivo).
+
+number_pattern deve usar um loop for.
+
+number_pattern(n) deve retornar uma string com todos os inteiros começando de 1 até n (incluído) separados por um espaço. Por exemplo, number_pattern(4) deve retornar a string 1 2 3 4.
+
+Se o argumento passado para a função não for um valor inteiro, a função deve retornar Argument must be an integer value.
+
+Se o argumento passado para a função for menor que 1, a função deve retornar Argument must be an integer greater than 0.
+
+Testes:
+
+    1. Você deve ter uma função number_pattern.
+    2. A função number_pattern deve ter um parâmetro chamado n.
+    3. number_pattern(4) deve retornar 1 2 3 4.
+    4. number_pattern(12) deve retornar 1 2 3 4 5 6 7 8 9 10 11 12.
+    5. number_pattern deve retornar uma lista de números separados por espaço para qualquer número inteiro positivo.
+    6. number_pattern deve retornar Argumento deve ser um valor inteiro. quando for passado um valor que não seja um inteiro.
+    7. number_pattern deve retornar Argumento deve ser um inteiro maior que 0. quando receber um inteiro não positivo.
+"""
