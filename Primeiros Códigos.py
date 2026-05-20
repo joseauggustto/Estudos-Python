1ca99b16c02d2f2e2b3c9fe792b1dadc02e4b829
joseauggustto <jose.morais22aluno@gmail.com>
2026-05-20 15:21:36 -0300

    Voltando...

---
+++ Primeiros Códigos.py
@@ -0,0 +1,24 @@
+# Ok, let's go! 
+
+print("Hello, World!")
+
+# nome = 'José Augusto'
+# idade = 22
+# idade1 = '22' 
+
+# print(nome + str(idade)) # Concatenando string com número, é necessário converter o número para string
+# print(nome + idade1)
+# # ############################################
+
+# name = "José Augusto"
+# print(name, type(name))
+
+# is_student = True
+# print(is_student, type(is_student))
+
+# age = 20
+# print(age, type(age))
+
+# score = 80.5
+# print(isinstance(score, float))
+# print(score, type(score))
