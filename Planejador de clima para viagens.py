7ad40335cca9056c4816e8962138d03e853a0de5
joseauggustto <jose.morais22aluno@gmail.com>
2026-05-27 21:04:44 -0300

    Noite de quarta

--- Planejador de clima para viagens.py
+++ Planejador de clima para viagens.py
@@ -1,38 +1,36 @@
 #Vamos lá...
-distance_mi =  0.9
+ 
+from sys import is_stack_trampoline_active
+distance_mi = 10 
 is_raining = True
-has_bike = True 
-has_car = False 
-has_ride_share_app = True 
+has_bike = True
+has_car = False
+has_ride_share_app = True
 
-# --------------------------
-
-# Curta distância - até 1 milha. 
 if distance_mi == 0:
-    print(False)
+    print("False")
+
 elif distance_mi <= 1:
+
     if is_raining == True:
-        print(False)
+        print("False")
     elif is_raining == False:
-        print(True)
+        print("True")
 
-# Média distância 
+# Média distância
 
-if distance_mi > 1 and distance_mi <= 6:
+if distance_mi > 1 and distance_mi <= 6: 
     if is_raining == True and has_bike == False:
-       print(False)
-    elif is_raining == True and has_bike == False: 
-        print(False)
+        print("False")
+    elif is_raining == False and has_bike == False:
+        print("False")
     elif has_bike == True and is_raining == False:
-        print(True)
+        print("True")
     
-    
-# Longa distância
-
 if distance_mi > 6:
     if has_ride_share_app == True:
-        print(True)
+        print("True")
     elif has_car == True:
-        print(True)
-    elif has_car == False and has_ride_share_app == False:
-        print(False)
+        print("True")
+    else:
+        print("False")
