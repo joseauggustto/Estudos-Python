3fbc64dca7b80aa9bb9e22a68db48b257e99e0c9
joseauggustto <jose.morais22aluno@gmail.com>
2026-05-30 11:17:42 -0300

    Manhã de Sábado

---
+++ Calculadora de Descontos.py
@@ -0,0 +1,22 @@
+# here go again
+
+def apply_discount(price, discount):
+
+    # O erro está nesta linha abaixo:
+    if not isinstance(price, (int, float)):
+        return "The price should be a number."
+    # E o mesmo erro se repete nesta linha:
+    elif not isinstance(discount, (int, float)):
+        return "The discount should be a number."
+    elif price <= 0:
+        return "The price should be greater than 0."
+    elif discount < 0 or discount > 100:
+        return "The discount should be between 0 and 100."
+    else:
+        final_price = price - (price * discount / 100)
+        return final_price
+
+
+teste1 = apply_discount(100, 20)
+print(teste1)
+
