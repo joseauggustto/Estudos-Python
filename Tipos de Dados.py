7de5335a04a0fd3d696fb1279e118b5f4801f667
joseauggustto <jose.morais22aluno@gmail.com>
2026-06-24 14:40:30 -0300

    Revisão

---
+++ Tipos de Dados.py
@@ -0,0 +1,53 @@
+#Aqui está uma lista dos possíveis tipos de dados em Python: 
+
+#Inteiro
+inteiro = 10
+print(type(inteiro)) 
+
+#Float
+decimal = 10.5
+print(type(decimal))
+
+#String
+string = 'José Augusto'
+print(type(string))
+
+#Booleano 
+boleano = True
+print(type(boleano))
+
+#Set (???) Coleção não ordenada de elementos únicos.
+set_var = {1, 2, 3}
+print(type(set_var))
+
+#Dicionário (Coleção de pares chave-valor)
+Dicionário = {'nome': 'José', 'idade': 30}
+print(type(Dicionário))
+
+#Tupla (Coleção ordenada e imutável)
+Tupla = (1, 2, 3)
+print(type(Tupla))
+
+#Range (Sequência de números)
+range = range(10)
+print(type(range))
+
+#Lista (Coleção ordenada e mutável)
+Lista = [1, 2, 3]
+print(type(Lista))
+
+#NoneType
+NoneType = None
+print(type(NoneType))
+
+# -------------------
+
+#isistance() usado para verificar se um dado é do tipo pré definido. 
+
+var = 10
+print(isinstance(var, int)) 
+
+# Acessando caracteres de uma string: 
+
+varia = 'José'
+print(varia[3])
